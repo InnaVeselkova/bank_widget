40d078e53af0e2f5783eccee6e9e9a6c549510bd
Inna <MenelN@yandex.ru>
2025-07-05 20:39:22 +0300

    edit tests for date

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -26,19 +26,24 @@ def test_mask_account_card(input_str, expected_output):
 def test_valid_date():
     assert get_date("2023-10-05T14:30:00") == "05.10.2023"
 
+
 def test_date_with_time():
-    assert get_date("2022-01-01T00:00:00") == "01.01.2022"
+    assert get_date("2022-01-01T00:00:00") ==
+
 
 def test_date_without_time():
     assert get_date("2021-12-31") == "31.12.2021"
+
 
 def test_invalid_date_format():
     with pytest.raises(ValueError):
         get_date("2023/10/05")  # неправильный формат
 
+
 def test_empty_date_string():
     with pytest.raises(ValueError):
         get_date("")
+
 
 def test_non_iso_format():
     with pytest.raises(ValueError):
