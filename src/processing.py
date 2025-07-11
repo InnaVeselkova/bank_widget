34d581be3bd5f35af36e64594bc40819cdc238c2
Inna <MenelN@yandex.ru>
2025-07-11 18:26:57 +0300

    add correction

--- src/processing.py
+++ src/processing.py
@@ -14,6 +14,7 @@ def sort_by_date(records: List[Dict[str, Any]], reverse: bool = True) -> List[Di
         raise TypeError("Некорректный формат данных")
 
 
+if __name__ == '__main__':
 records = [
         {'id': 1, 'date': '01/11/2025'},
         {'id': 2, 'date': '2025-11-07'},
