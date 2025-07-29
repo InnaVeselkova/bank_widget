e1eec6b451c6fca966d403d3dcad683451918878
Inna <MenelN@yandex.ru>
2025-07-29 16:26:05 +0300

    add logs

--- src/masks.py
+++ src/masks.py
@@ -1,20 +1,39 @@
+import logging
+
+# Получение корневого логера
+root_logger = logging.getLogger()
+
+# Создание и получение именованного логера
+app_logger_masks = logging.getLogger("my_lg_masks")
+
+file_handler = logging.FileHandler('../logs/masks.log', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+app_logger_masks.addHandler(file_handler)
+app_logger_masks.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """функция отображения маски номера карты"""
+    app_logger_masks.info(f"Вызов get_mask_card_number с номером: {card_number}")
 
     """Проверка корректности символов номера"""
     if not all(c.isdigit() or c.isspace() for c in card_number):
+        app_logger_masks.warning("Некорректные символы в номере карты")
         return "Ошибка: номер карты содержит недопустимые символы."
 
     """Удаляем все пробелы и другие нецифровые символы"""
     digits = "".join(filter(str.isdigit, card_number))
+    app_logger_masks.debug(f"Обработанные цифры карты: {digits}")
 
     """Проверка длины номера"""
     if len(digits) != 16:
+        app_logger_masks.warning(f"Недопустимая длина номера карты: {len(digits)}")
         return "Недопустимый номер карты"
 
     """Маскируем цифры посередине"""
     masked_digits_card_number = digits[:4] + ' ' + digits[4:6] + "*" * 2 + ' ' + "*" * 4 + ' ' + digits[-4:]
-
+    app_logger_masks.info(f"Маскированный номер карты: {masked_digits_card_number}")
     return masked_digits_card_number
 
 
@@ -23,16 +42,26 @@ def get_mask_account(account_number: str) -> str:
 
     """Проверка корректности символов номера"""
     if not all(c.isdigit() or c.isspace() for c in account_number):
+        app_logger_masks.warning("Некорректные символы в номере счета")
         return "Ошибка: номер счета содержит недопустимые символы."
 
     """Удаляем все пробелы и другие нецифровые символы"""
     digits = "".join(filter(str.isdigit, account_number))
+    app_logger_masks.debug(f"Обработанные цифры счета: {digits}")
 
     """Проверка длины номера счета"""
     if len(digits) != 20:
+        app_logger_masks.warning(f"Недопустимая длина номера счета: {len(digits)}")
         return "Недопустимый номер счета"
 
     """Маскируем цифры и убираем лишние"""
     masked_account_number = "**" + digits[-4:]
-
+    app_logger_masks.info(f"Маскированный номер счета: {masked_account_number}")
     return masked_account_number
+
+
+if __name__ == '__main__':  # pragma: no cover
+    get_mask_card_number("1234567890123456")
+    get_mask_account("11223344556677889900")
+    get_mask_card_number("12345890123456")
+    get_mask_account("1122334455667789900")
--- src/utils.py
+++ src/utils.py
@@ -1,45 +1,85 @@
 import json
 import os
 from typing import Dict
+import logging
+
+# Получение корневого логера
+root_logger = logging.getLogger()
+
+# Создание и получение именованного логера
+app_logger_utils = logging.getLogger("my_lg_utils")
+
+file_handler = logging.FileHandler('../logs/utils.log', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+app_logger_utils.addHandler(file_handler)
+app_logger_utils.setLevel(logging.DEBUG)
 
 
 def load_transactions(json_path):
-    # Загружает данные о финансовых операциях из файла JSON
+    """ Загружает данные о финансовых операциях из файла JSON """
+    app_logger_utils.info(f"Загрузка транзакций из файла: {json_path}")
     if not os.path.exists(json_path):
         # Выводит пустой список в случае отсутствия файла
+        app_logger_utils.warning(f"Файл не найден: {json_path}. Возвращаю пустой список.")
         return []
 
     try:
         with open(json_path, 'r', encoding='utf-8') as f:
             data = json.load(f)
             if isinstance(data, list):
+                app_logger_utils.info(f"Успешно загружено {len(data)} транзакций.")
                 return data
             else:
+                app_logger_utils.warning("Данные в файле не являются списком.")
                 # Не список
                 return []
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
+        app_logger_utils.error(f"Ошибка декодирования JSON: {e}")
         # Ошибка чтения файла
         return []
 
 
 def get_transaction_amount_in_rub(transaction: Dict) -> float:
-    # Функция, которая принимает на вход транзакцию и возвращает сумму транзакции в рублях
+    """  Функция, которая принимает на вход транзакцию и возвращает сумму транзакции в рублях """
+    app_logger_utils.info(f"Обработка транзакции: {transaction}")
 
     amount = transaction.get("operationAmount", {}).get('amount', 0)
+    app_logger_utils.debug(f"Исходная сумма: {amount}")
 
     if amount == ' ':
+        app_logger_utils.warning(f"Пустая сумма для транзакции: {transaction}. Возвращаю 0.")
         return 0
 
     currency = transaction.get("operationAmount", {}).get('currency', {}).get('code')
+    app_logger_utils.info(f"Валюта транзакции: {transaction}: {currency}")
 
     if currency == 'RUB':
+        app_logger_utils.info(f"Для транзакции {transaction} сумма в рублях {float(amount)}")
         return float(amount)
     else:
+        app_logger_utils.info(f"Транзакция {transaction} в валюте {currency}")
         from external_api import convert_to_rub
         return convert_to_rub(currency) * float(amount)
 
 
 if __name__ == '__main__':  # pragma: no cover
+
+    print(get_transaction_amount_in_rub({
+        "id": 641945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }))
 
     print(get_transaction_amount_in_rub({
         "id": 441945886,
@@ -49,7 +89,7 @@ if __name__ == '__main__':  # pragma: no cover
             "amount": "31957.58",
             "currency": {
                 "name": "руб.",
-                "code": "USD"
+                "code": "RUB"
             }
         },
         "description": "Перевод организации",
